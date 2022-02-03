f11cfe78ac7add74d981fef8184c3ea50e3c0314
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2022-02-03 10:34:33 +0330

    a

--- js/header.js
+++ js/header.js
@@ -1,44 +1,15 @@
-let header_hamIco = document.querySelector(".header_hamIco");
-let header_line = document.querySelector(".header_line");
-let header_popup = document.querySelector(".header_popup");
-let header_close = document.querySelector(".header_close");
-let toolBar_logo = document.querySelector(".toolBar_logo");
-let body = document.querySelector("body");
+//hamburger icon toggle
+let header_hamIco = document.querySelector(".header-hamIco");
+let header_line = document.querySelector(".header-hamIco-line");
 let menu_is_open = false;
-
 header_hamIco.addEventListener("click", () => {
-  header_popup.classList.toggle("header_popup--open");
-  header_line.classList.toggle("header_line--open");
-  header_hamIco.classList.toggle("header_hamIco--open");
-  header_close.classList.toggle("header_close--open");
+  header_line.classList.toggle("header-hamIco-line--open");
   menu_is_open = true;
 });
-header_popup.addEventListener("click", (e) => {
-  e.stopPropagation();
+
+///translate switch toggle
+let header_switch = document.querySelector(".header-translate-switch");
+let header_pin = document.querySelector(".header-translate-pin");
+header_switch.addEventListener("click", () => {
+  header_pin.classList.toggle("header-translate-pin--active");
 });
-header_close.addEventListener(
-  "click",
-  () => {
-    if ((menu_is_open = true)) {
-      header_popup.classList.remove("header_popup--open");
-      header_line.classList.remove("header_line--open");
-      header_hamIco.classList.remove("header_hamIco--open");
-      header_close.classList.remove("header_close--open");
-      menu_is_open = false;
-    }
-  },
-  true
-);
-
-$(window).on("scroll", _.throttle(onScroll, 1000, { trailing: false }));
-function onScroll() {
-  if ((menu_is_open = true)) {
-    header_popup.classList.remove("header_popup--open");
-    header_line.classList.remove("header_line--open");
-    header_hamIco.classList.remove("header_hamIco--open");
-    header_close.classList.remove("header_close--open");
-    menu_is_open = false;
-  }
-}
-
-//index
